b234f0c41967301e8559f1866638e93f512407e2
JPol0 <160655545+JPol0@users.noreply.github.com>
2025-06-27 19:56:23 -0400

    fix: add imagen default cuando la serie no tiene logo

--- scripts/functionSeries.js
+++ scripts/functionSeries.js
@@ -26,27 +26,52 @@ $(document).ready(function()
         {
             // Obtener todas las series de cartas
             const series = await tcgdex.fetch('series');;
+            console.log(series)
             
             $("#list-series").empty()
 
             for(let serie of series)
             {
-                $("#list-series").append
-                (
-                    `<div class="row mb-4 d-flex g-2 g-sm-0 align-items-center justify-content-center">
+                if(!serie.logo)
+                {
+                    $("#list-series").append
+                    (
+                        `<div class="row mb-4 d-flex g-2 g-sm-0 align-items-center justify-content-center">
 
-                        <div class="col-12 col-sm-3 text-center">
-                            <!-- imagen de la serie -->
-                            <img class="rounded-4 d-inline imgSerie" src=${serie.logo}.png alt="">
-                        </div>
+                            <div class="col-12 col-sm-3 text-center">
+                                <!-- imagen de la serie -->
+                                <span class="material-symbols-outlined rounded-4 d-inline iconSerie">bid_landscape_disabled</span>
+                            </div>
 
-                        <div class="col-12 col-sm-9 text-center text-md-start">
-                            <!-- nombre de la serie -->
-                            <p class="text-white m-0 h2 mb-1 d-inline">${serie.name}</p>
-                        </div>
-                
-                    </div>`
-                )
+                            <div class="col-12 col-sm-9 text-center text-md-start">
+                                <!-- nombre de la serie -->
+                                <p class="text-white m-0 h2 mb-1 d-inline">${serie.name}</p>
+                            </div>
+                            
+                        </div>`
+                    )
+                }
+                else
+                {
+                    $("#list-series").append
+                    (
+                        `<div class="row mb-4 d-flex g-2 g-sm-0 align-items-center justify-content-center">
+    
+                            <div class="col-12 col-sm-3 text-center">
+                                <!-- imagen de la serie -->
+                                <img class="rounded-4 d-inline imgSerie" src=${serie.logo}.png alt="">
+                            </div>
+    
+                            <div class="col-12 col-sm-9 text-center text-md-start">
+                                <!-- nombre de la serie -->
+                                <p class="text-white m-0 h2 mb-1 d-inline">${serie.name}</p>
+                            </div>
+                    
+                        </div>`
+                    )
+
+                }
+
             }
         }
         catch
